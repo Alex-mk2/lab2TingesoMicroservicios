bc6d826ff2a771c6682ac70cc26c4be77344c12d
Alex <alex.mellado.g@usach.cl>
2023-11-10 22:39:43 -0300

    [Alex Mellado]-Correcion cuotas service (duplicacion de archivos)

--- eureka-service/src/main/java/com/tutorial/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/tutorial/eurekaservice/EurekaServiceApplication.java
@@ -1,5 +1,4 @@
 package com.tutorial.eurekaservice;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
