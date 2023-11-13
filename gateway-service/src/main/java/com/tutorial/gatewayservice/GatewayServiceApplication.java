4000d57551ebfceeb784684b80b983efb2fec6ad
Alex-mk2 <75042499+Alex-mk2@users.noreply.github.com>
2023-11-13 16:05:16 -0300

    Actualizacion gateway

--- gateway-service/src/main/java/com/tutorial/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/tutorial/gatewayservice/GatewayServiceApplication.java
@@ -1,5 +1,4 @@
 package com.tutorial.gatewayservice;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
