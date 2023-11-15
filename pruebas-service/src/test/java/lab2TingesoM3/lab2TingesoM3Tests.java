0660f100483dfd0f0dd02fef3f4c423ebcd147d4
Alex-mk2 <75042499+Alex-mk2@users.noreply.github.com>
2023-11-15 18:35:21 -0300

    Carpeta

---
+++ pruebas-service/src/test/java/lab2TingesoM3/lab2TingesoM3Tests.java
@@ -0,0 +1,13 @@
+package lab2TingesoM3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class lab2TingesoM3Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
