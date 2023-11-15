ec1aaa554bb41b086a0bc941c390b936db9955b0
Alex-mk2 <75042499+Alex-mk2@users.noreply.github.com>
2023-11-15 18:33:34 -0300

    Carpeta

---
+++ estudiante-service/src/test/java/lab2TingesoM1/estudianteEntityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lab2TingesoM1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class estudianteEntityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
